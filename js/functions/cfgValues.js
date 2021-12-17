ef211f7340a198e2a7436576c7933553e80cf8f5
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-17 17:05:35 -0500

    pega valores de formularios

---
+++ js/functions/cfgValues.js
@@ -0,0 +1,9 @@
+function getSetingValue() {
+    let p1_name = document.getElementById('p1-name').value;
+    let p2_name = document.getElementById('p2-name').value;
+
+    return {
+        p1_name: p1_name,
+        p2_name: p2_name
+    }
+}
