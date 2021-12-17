e028a4a5a9d08e4f089b91dffd7e777ed66fc397
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-17 17:05:06 -0500

    arquivo que ficara as variaveis

---
+++ js/script.js
@@ -0,0 +1,26 @@
+let jogador1 = {
+    nome: "P1",
+    forma: "x",
+    postos: 0
+};
+
+let jogador2 = {
+    nome: "P2",
+    forma: "o",
+    postos: 0
+};
+
+let gameStartState = false;
+let jogadorAtual = jogador1;
+let preenchido = [9];
+let tabuleiro = [9];
+
+
+
+
+
+
+
+
+
+
