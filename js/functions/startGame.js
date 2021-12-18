61150121b17796600aa0cbf6c2eb931851a9d312
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-18 18:53:24 -0500

    troca de class ao iniciar o jogo para fazer a animação

--- js/functions/startGame.js
+++ js/functions/startGame.js
@@ -1,4 +1,12 @@
 function startGame() {
+    let navMenu = document.querySelector('.menuCenter');
+    navMenu.classList.remove('menuCenter');
+    navMenu.classList.add('menuLeft');
+
+    let tabuleiro = document.querySelector('.jogo');
+    tabuleiro.classList.remove('ocuto');
+    tabuleiro.classList.add('ativo');
+ 
     let cfg = getSetingValue();
     jogador1.nome = cfg.p1_name;
     jogador2.nome = cfg.p2_name;
