a9b08a785db54950205232906e112ecf00532c38
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-18 18:53:56 -0500

    adicionado um nome padão para os jogadores se não preencher o formulario

--- js/functions/cfgValues.js
+++ js/functions/cfgValues.js
@@ -2,6 +2,12 @@ function getSetingValue() {
     let p1_name = document.getElementById('p1-name').value;
     let p2_name = document.getElementById('p2-name').value;
 
+    if(p1_name.value == undefined || p2_name == undefined) {
+        return { 
+            p1_name: 'P1',
+            p2_name: 'p2'
+        }
+    }
     return {
         p1_name: p1_name,
         p2_name: p2_name
