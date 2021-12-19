f478a8aa4b4e098abf4756e3a9fc161395c0f4e6
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-18 20:13:48 -0500

    valida o ganhador independente da peça

--- js/functions/regras.js
+++ js/functions/regras.js
@@ -5,107 +5,107 @@
 function regras() {
     // ganha X
     // regras para ganha em linhas
-    if (tabuleiro[1] === "x" &&
-        tabuleiro[2] === "x" &&
-        tabuleiro[3] === "x") {
-        ganhador("x");
+    if (tabuleiro[1] === jogador1.forma &&
+        tabuleiro[2] === jogador1.forma &&
+        tabuleiro[3] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
-    if (tabuleiro[4] === "x" &&
-        tabuleiro[5] === "x" &&
-        tabuleiro[6] === "x") {
-        ganhador("x");
+    if (tabuleiro[4] === jogador1.forma &&
+        tabuleiro[5] === jogador1.forma &&
+        tabuleiro[6] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
-    if (tabuleiro[7] === "x" &&
-        tabuleiro[8] === "x" &&
-        tabuleiro[9] === "x") {
-        ganhador("x");
+    if (tabuleiro[7] === jogador1.forma &&
+        tabuleiro[8] === jogador1.forma &&
+        tabuleiro[9] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
     // ganha O
-    if (tabuleiro[1] === "o" &&
-        tabuleiro[2] === "o" &&
-        tabuleiro[3] === "o") {
-        ganhador("o");
+    if (tabuleiro[1] === jogador2.forma &&
+        tabuleiro[2] === jogador2.forma &&
+        tabuleiro[3] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
-    if (tabuleiro[4] === "o" &&
-        tabuleiro[5] === "o" &&
-        tabuleiro[6] === "o") {
-        ganhador("o");
+    if (tabuleiro[4] === jogador2.forma &&
+        tabuleiro[5] === jogador2.forma &&
+        tabuleiro[6] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
-    if (tabuleiro[7] === "o" &&
-        tabuleiro[8] === "o" &&
-        tabuleiro[9] === "o") {
-        ganhador("o");
+    if (tabuleiro[7] === jogador2.forma &&
+        tabuleiro[8] === jogador2.forma &&
+        tabuleiro[9] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
     // regras para ganha em colunas
 
-    if (tabuleiro[1] === "x" &&
-        tabuleiro[4] === "x" &&
-        tabuleiro[7] === "x") {
-        ganhador("x");
+    if (tabuleiro[1] === jogador1.forma &&
+        tabuleiro[4] === jogador1.forma &&
+        tabuleiro[7] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
-    if (tabuleiro[2] === "x" &&
-        tabuleiro[5] === "x" &&
-        tabuleiro[8] === "x") {
-        ganhador("x");
+    if (tabuleiro[2] === jogador1.forma &&
+        tabuleiro[5] === jogador1.forma &&
+        tabuleiro[8] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
-    if (tabuleiro[3] === "x" &&
-        tabuleiro[6] === "x" &&
-        tabuleiro[9] === "x") {
-        ganhador("x");
+    if (tabuleiro[3] === jogador1.forma &&
+        tabuleiro[6] === jogador1.forma &&
+        tabuleiro[9] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
     // ganha O
-    if (tabuleiro[1] === "o" &&
-        tabuleiro[4] === "o" &&
-        tabuleiro[7] === "o") {
-        ganhador("o");
+    if (tabuleiro[1] === jogador2.forma &&
+        tabuleiro[4] === jogador2.forma &&
+        tabuleiro[7] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
-    if (tabuleiro[2] === "o" &&
-        tabuleiro[5] === "o" &&
-        tabuleiro[8] === "o") {
-        ganhador("o");
+    if (tabuleiro[2] === jogador2.forma &&
+        tabuleiro[5] === jogador2.forma &&
+        tabuleiro[8] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
-    if (tabuleiro[3] === "o" &&
-        tabuleiro[6] === "o" &&
-        tabuleiro[9] === "o") {
-        ganhador("o");
+    if (tabuleiro[3] === jogador2.forma &&
+        tabuleiro[6] === jogador2.forma &&
+        tabuleiro[9] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
 
     // regras para ganha em diagonais
 
     // ganha X
-    if (tabuleiro[1] === "x" &&
-        tabuleiro[5] === "x" &&
-        tabuleiro[9] === "x") {
-        ganhador("x");
+    if (tabuleiro[1] === jogador1.forma &&
+        tabuleiro[5] === jogador1.forma &&
+        tabuleiro[9] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
-    if (tabuleiro[3] === "x" &&
-        tabuleiro[5] === "x" &&
-        tabuleiro[7] === "x") {
-        ganhador("x");
+    if (tabuleiro[3] === jogador1.forma &&
+        tabuleiro[5] === jogador1.forma &&
+        tabuleiro[7] === jogador1.forma) {
+        ganhador(jogador1.forma);
     }
 
     // ganha O
-    if (tabuleiro[1] === "o" &&
-        tabuleiro[5] === "o" &&
-        tabuleiro[9] === "o") {
-        ganhador("o");
+    if (tabuleiro[1] === jogador2.forma &&
+        tabuleiro[5] === jogador2.forma &&
+        tabuleiro[9] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
 
-    if (tabuleiro[3] === "o" &&
-        tabuleiro[5] === "o" &&
-        tabuleiro[7] === "o") {
-        ganhador("o");
+    if (tabuleiro[3] === jogador2.forma &&
+        tabuleiro[5] === jogador2.forma &&
+        tabuleiro[7] === jogador2.forma) {
+        ganhador(jogador2.forma);
     }
     else {
         ganhador("velha");
