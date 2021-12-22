81c9a3e8e2a15aa3bd84f748a790d3fd193b6ae4
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-22 18:11:12 -0300

    arquivo index que sera chamado a class Game

---
+++ js/index.js
@@ -0,0 +1,18 @@
+let game = new Game();
+
+
+function startGame() {
+    game.startGame();
+}
+
+function boardHome(input,board_home) {
+    game.startGame();
+    game.playerMove(input,board_home);
+    console.log(game.currentPlayer);
+}
+
+
+
+
+
+
