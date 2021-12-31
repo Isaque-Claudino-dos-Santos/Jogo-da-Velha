245769eea56eaae8853a09fcaf4691d465da4377
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-31 17:21:05 -0300

    file the burguer button on e off e the form add e rmove classes

--- js/burguer-button/index.js
+++ js/burguer-button/index.js
@@ -1,6 +1,23 @@
 let burger_button = document.querySelector('.burger_button');
+let form_config = document.querySelector('.form_container .form_state_off');
 
+let btnState = true;
 burger_button.onclick = () => {
-    burger_button.classList.remove('off');
-    burger_button.classList.add('on');
+    if(!btnState) {
+        burger_button.classList.remove('off');
+        burger_button.classList.add('on');
+
+        form_config.classList.remove('form_state_off');
+        form_config.classList.add('form_state_on');
+
+        btnState = true;
+    }else {
+        burger_button.classList.add('off');
+        burger_button.classList.remove('on');
+
+        form_config.classList.remove('form_state_on');
+        form_config.classList.add('form_state_off');
+
+        btnState = false;
+    }
 }
