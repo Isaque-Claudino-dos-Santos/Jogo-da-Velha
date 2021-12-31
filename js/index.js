d7141d3795369c681a4267dc61594c7051bc4aac
Isaque-Claudino-dos-Santos <isaqueclaudino12@gmail.com>
2021-12-31 17:59:40 -0300

    add troca de css do main

--- js/index.js
+++ js/index.js
@@ -1,13 +1,24 @@
 let game = new Game();
 
+window.onload = () => {
+    if (!game.gameStart) {
+        let main_game = document.querySelector('main');
+        main_game.classList.remove('on')
+        main_game.classList.add('off')
+    }
+}
 
 function startGame() {
+    game.gameStart = true;
+    let main_game = document.querySelector('main');
+    main_game.classList.remove('off')
+    main_game.classList.add('on')
     game.setValues();
 }
 
-function returnBoardHome(input,board_home) {
+function returnBoardHome(input, board_home) {
     game.setValues();
-    game.playerMove(input,board_home);
+    game.playerMove(input, board_home);
 }
 
 
